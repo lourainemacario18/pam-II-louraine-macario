5e30324977850e8fb7a31076c67f8d6635513934
Louraine Macario <louraine.macario18@gmail.com>
2024-04-10 11:35:43 -0300

    Criação do primeiro arquivo Java Script

---
+++ aula01/metodosevariaveis.js
@@ -0,0 +1,15 @@
+// variavel global
+var variavel1 = "olá";
+
+// variavel local
+var variavel2 = 3;
+
+// Constance
+const variavel3 =8;
+
+//Métodos já vem configurados
+console.log(
+    "Olá mundo!"
+);
+
+console.log(variavel1, "mundo!");
